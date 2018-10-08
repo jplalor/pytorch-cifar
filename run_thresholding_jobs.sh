468bf16916708c8cc260be5d94129431fd87f089
John Lalor <lalor@cs.umass.edu>
2018-10-08 06:50:15 -0400

    fix shell script

--- run_thresholding_jobs.sh
+++ run_thresholding_jobs.sh
@@ -2,11 +2,11 @@
 
 
 # test first
-sbatch -p titanx-long --gres=gpu:1 --mem=80000 --output=logs/cifar_threshold_$t.log --wrap="python -u main.py --threshold -1 --diff-dir /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/ --threshold-type 1 -p /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/thresholds/"
+sbatch -p titanx-short --gres=gpu:2 --mem=80000 --output=logs/cifar_threshold_$t.log --wrap="python -u main.py --threshold -1 --diff-dir /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/ --threshold-type 1 -p /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/thresholds/"
 
 #for t in $(seq -6 0.1 6)
 #do
-#sbatch -p titanx-long --gres=gpu:1 --mem=80000 --output=logs/mnist_threshold_$t.log --wrap="python -u main.py --threshold -1 --diff-dir /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/ --threshold-type 1 -p /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/thresholds/"
-#sbatch -p titanx-long --gres=gpu:1 --mem=80000 --output=logs/mnist_threshold_upper_bound_$t.log --wrap="python -u main.py --threshold -1 --diff-dir /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/ --threshold-type 2 -p /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/thresholds/"
-#sbatch -p titanx-long --gres=gpu:1 --mem=80000 --output=logs/mnist_threshold_lower_bound_$t.log --wrap="python -u main.py --threshold -1 --diff-dir /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/ --threshold-type 3 -p /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/thresholds/"
+#sbatch -p titanx-short --gres=gpu:2 --mem=80000 --output=logs/cifar_threshold_$t.log --wrap="python -u main.py --threshold -1 --diff-dir /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/ --threshold-type 1 -p /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/thresholds/"
+#sbatch -p titanx-short --gres=gpu:2 --mem=80000 --output=logs/cifar_threshold_upper_bound_$t.log --wrap="python -u main.py --threshold -1 --diff-dir /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/ --threshold-type 2 -p /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/thresholds/"
+#sbatch -p titanx-short --gres=gpu:2 --mem=80000 --output=logs/cifar_threshold_lower_bound_$t.log --wrap="python -u main.py --threshold -1 --diff-dir /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/ --threshold-type 3 -p /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/thresholds/"
 #done
