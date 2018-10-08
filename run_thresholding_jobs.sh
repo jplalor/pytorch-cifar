22674d63a63fc4a795fada3aa6a916c47eceea44
John Lalor <lalor@cs.umass.edu>
2018-10-08 07:00:57 -0400

    bugfix

--- run_thresholding_jobs.sh
+++ run_thresholding_jobs.sh
@@ -2,7 +2,7 @@
 
 
 # test first
-sbatch -p titanx-short --gres=gpu:2 --mem=80000 --output=logs/cifar_threshold_$t.log --wrap="python -u main.py --threshold -1 --diff-dir /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/ --threshold-type 1 -p /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/thresholds/"
+sbatch -p titanx-short --gres=gpu:2 --mem=80000 --output=logs/cifar_threshold_$t.log --wrap="python -u main.py --threshold 2 --diff-dir /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/ --threshold-type 1 -p /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/thresholds/"
 
 #for t in $(seq -6 0.1 6)
 #do
