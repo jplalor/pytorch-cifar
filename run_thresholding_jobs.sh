aa2f8eb328653d7185f216df69b8fabce91a3eba
John Lalor <lalor@cs.umass.edu>
2018-10-09 12:47:55 -0400

    don't need to run the baseline again

--- run_thresholding_jobs.sh
+++ run_thresholding_jobs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # baseline
-sbatch -p 1080ti-short --gres=gpu:2 --mem=80000 --output=logs/cifar_threshold_baseline.log --wrap="python -u main.py --threshold 0 --diff-dir /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/ --threshold-type 0 -p /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/thresholds/"
+#sbatch -p 1080ti-short --gres=gpu:2 --mem=80000 --output=logs/cifar_threshold_baseline.log --wrap="python -u main.py --threshold 0 --diff-dir /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/ --threshold-type 0 -p /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/thresholds/"
 
 # test first
 #sbatch -p titanx-short --gres=gpu:2 --mem=80000 --output=logs/cifar_threshold_test.log --wrap="python -u main.py --threshold 2 --diff-dir /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/ --threshold-type 1 -p /mnt/nfs/work1/hongyu/lalor/data/irt-svi/CIFAR/thresholds/"
